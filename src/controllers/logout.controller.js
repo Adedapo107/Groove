e640ff6f57314835d57c97ee62bfb3867956adfd
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2024-11-12 17:57:57 +0000

    Added logout page and 404 page

---
+++ src/controllers/logout.controller.js
@@ -0,0 +1,14 @@
+/**
+ * @license Apache-2.0
+ * @copyright Adedapo 2024
+ */
+
+"use strict";
+
+const logout = (req, res) => {
+  res.clearCookie('access_token');
+  res.clearCookie('refresh_token'); 
+  res.redirect('/login');
+};
+
+module.exports = { logout };
